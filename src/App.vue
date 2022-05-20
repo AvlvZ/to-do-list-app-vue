aafb408f798ba5d3cb559b1f450ce337ebab2506
AvlvZ <avlakov58@mail.ru>
2022-05-20 19:29:34 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,108 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>To Do Application</h1>
+    <hr>
+    <div class="backlogs">
+      <ul class="backlog__list">
+        <BacklogListItem :posts="posts"/>
+      </ul>
+    </div>
+    <form class="add-backlog">
+      <input v-bind:value="inputValue" @input="createInput" type="text" class="add-backlog__input" placeholder="Добавить задачу">
+      <input @click="addBacklog" type="button" class="add-backlog__submit" value="Добавить">
+    </form>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import BacklogListItem from "@/components/BacklogListItem";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: {BacklogListItem},
+  data() {
+    return {
+      posts: [
+
+      ],
+      inputValue: ""
+    }
+  },
+  methods: {
+    addBacklog() {
+      const newPost = {
+        id: Date.now(),
+        value: this.inputValue
+      };
+      this.posts.push(newPost);
+      this.inputValue = "";
+    },
+    createInput(event) {
+      this.inputValue = event.target.value;
+    }
   }
 }
 </script>
 
 <style>
+* {
+  text-align: left;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background: #d8d8d8;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  max-width: 1920px;
+  margin: 0 auto;
+  overflow: hidden;
+}
+
+h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 32px 0;
+}
+
+.backlogs {
+  width: 50%;
+  height: 500px;
+  max-width: 1200px;
+  background: #FFF;
+  padding: 32px;
+  border-radius: 8px;
+  margin: 32px auto;
+  overflow-y: scroll;
+}
+
+.backlog__list {
+  list-style: none;
+}
+
+.add-backlog {
+  display: flex;
+  gap: 5%;
+  width: 50%;
+  height: 40px;
+  max-width: 1200px;
+  border-radius: 16px;
+  margin: 32px auto;
+}
+
+.add-backlog__input {
+  width: 100%;
+  height: 100%;
+  padding: 0 2rem;
+  border: 2px solid #a4a4a4;
+}
+
+.add-backlog__submit {
+  padding: 0 2rem;
+  border: 2px solid #a4a4a4;
+  background: #FFF;
+  cursor: pointer;
 }
 </style>
---
+++ src/components/BacklogListItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <li class="backlog-list__item" v-for="post in posts" :key="post">
+    <div class="post">{{ post.value }}</div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style scoped>
+.backlog-list__item {
+  display: flex;
+  justify-content: space-between;
+  gap: 32px;
+}
+.backlog-list__item {
+  padding-bottom: 8px;
+  border-bottom: 2px solid #d8d8d8;
+}
+
+.backlog-list__item:not(:first-child) {
+  margin-top: 32px;
+}
+</style>
